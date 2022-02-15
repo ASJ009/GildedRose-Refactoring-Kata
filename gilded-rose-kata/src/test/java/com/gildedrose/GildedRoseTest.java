c739cc992a6b7743e67f2343b55fb1d199043f89
Amit Jadhav (OSV) <amit.jadhav.osv@fedex.com>
2022-02-15 13:35:47 +0100

    Test cases to check for new conjured item added

--- gilded-rose-kata/src/test/java/com/gildedrose/GildedRoseTest.java
+++ gilded-rose-kata/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -12,8 +12,40 @@ class GildedRoseTest {
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
-        app.updateQuality();
-        assertEquals(8, app.items[0].sellIn);
     }
 
+    /**
+     * Test case to check if the conjured items degrades quality twice as fast as normal items
+     * 
+     */
+    @Test
+    void testConjuredItems() {
+    	int quality = 10;
+        Item[] items = new Item[] { new Item("Conjured Mana Cake", 5, 10) };
+        GildedRose app = new GildedRose(items);
+        while(items[0].sellIn > 0) { 
+        	app.updateQuality();
+        	quality -= 2; // for every passing day the quality should be reduced by 2
+        	assertEquals(quality, app.items[0].quality);
+        }
+    }
+    
+    /**
+     * Test case to check if the conjured items quality is never negative
+     * 
+     */
+    @Test
+    void testConjuredItemsForMinimumQuality() {
+    	int quality = 4;
+        Item[] items = new Item[] { new Item("Conjured Mana Cake", 5, 4) };
+        GildedRose app = new GildedRose(items);
+        while(items[0].sellIn > 0) { 
+        	app.updateQuality();
+        	quality -= 2; // for every passing day the quality should be reduced by 2, once negative it should always be 0
+        	if(quality < 0) {
+        		quality = 0;
+        	}
+        	assertEquals(quality, app.items[0].quality);
+        }
+    }
 }
--- gilded-rose-kata/src/test/java/com/gildedrose/TexttestFixture.java
+++ gilded-rose-kata/src/test/java/com/gildedrose/TexttestFixture.java
@@ -5,25 +5,26 @@ public class TexttestFixture {
         System.out.println("OMGHAI!");
 
         Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                new Item("+5 Dexterity Vest", 2, 14), // once the sellIn in negative quality degrades twice as fast
+                new Item("Aged Brie", 2, 0), // increases in quality the older it gets
+                new Item("Elixir of the Mongoose", 5, 7), // quality is reduced by one as sellIn reduces
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), // quality is never altered
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80), // quality is never altered
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20), // quality increases by 1 when sellIn is greater than 10
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 30), // quality increases by 2 when sellIn is between 6-10
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 30), // quality increases by 3 when sellIn is greater than 0-5
+                new Item("Backstage passes to a TAFKAL80ETC concert", 2, 30), // quality drops to 0 when sellIn is less than 0
                 // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+                new Item("Conjured Mana Cake", 3, 6) }; // quality decreases twice as fast as normal items(ie. decreses by two every passig day)
 
         GildedRose app = new GildedRose(items);
 
-        int days = 2;
+        int days = 5;
         if (args.length > 0) {
             days = Integer.parseInt(args[0]) + 1;
         }
 
-        for (int i = 0; i < days; i++) {
+        for (int i = 0; i <= days; i++) {
             System.out.println("-------- day " + i + " --------");
             System.out.println("name, sellIn, quality");
             for (Item item : items) {
@@ -32,6 +33,13 @@ public class TexttestFixture {
             System.out.println();
             app.updateQuality();
         }
+        
+        System.out.println("After " + (days+1) + " days of processing");
+        System.out.println("name, sellIn, quality");
+        for (Item item : items) {
+            System.out.println(item);
+        }
+        System.out.println();
     }
 
 }
